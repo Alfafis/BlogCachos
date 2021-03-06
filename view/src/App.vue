e6078a62490dafff70e865ae932dace3b8d98275
Alfafis <ctpmmg2009_101@hotmail.com>
2021-03-06 20:28:35 -0300

    ajuste css nav

--- view/src/App.vue
+++ view/src/App.vue
@@ -1,10 +1,81 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link>
-
+    <Header />
+    <router-view />
+    <Footer />
+    <!-- <router-link to="/">Home</router-link> -->
   </div>
-  <router-view/>
 </template>
 
+<script>
+import Header from "@/components/header.vue";
+import Footer from "@/components/footer.vue";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,500,80");
+
+* {
+  box-sizing: border-box;
+}
+
+body,
+ul,
+li,
+h1,
+h2,
+p {
+  padding: 0;
+  margin: 0;
+}
+
+ul {
+  list-style: none;
+}
+
+body {
+  font-family: "Montserrat", Helvetica, sans-serif;
+  color: #345;
+  background: url("/assets/backgroung.jpg") repeat top;
+}
+
+a {
+  color: #345;
+  list-style: none;
+  text-decoration: none;
+}
+
+img {
+  max-width: 100%;
+  display: block;
+}
+
+.btn {
+  display: block;
+  padding: 0.625rem 1.875rem;
+  background: #87f;
+  border-radius: 4px;
+  color: #fff;
+  text-align: center;
+  font-size: 1rem;
+  font-weight: bold;
+  box-shadow: 0 4px 8px rgb(30, 60, 90, 0.2);
+  transition: all 0.3s;
+  border: none;
+  font-family: "Montserrat", Helvetica, sans-serif;
+  cursor: pointer;
+  margin-bottom: 0.325rem;
+}
+
+.btn:hover {
+  background: #65d;
+  transform: scale(1.1);
+}
 </style>
---
+++ view/src/components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <div class="logo-topo">
+      <img class="smart" src="@/assets/menu.png" alt="lorena-logo" />
+    </div>
+    <nav>
+      <router-link class="btn" to="/">Cabelos</router-link>
+      <router-link class="btn" to="/">Vídeos</router-link>
+      <router-link class="btn" to="/">Moda</router-link>
+      <router-link class="btn" to="/">Música</router-link>
+      <router-link class="btn" to="/">Dicas</router-link>
+      <router-link class="btn" to="/">Motivação</router-link>
+      <router-link class="btn" to="/">Contato</router-link>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style></style>
