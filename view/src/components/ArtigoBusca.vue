652ce5062ca7e7facc3c9f5f103f2fcb8cb95381
Alfafis <ctpmmg2009_101@hotmail.com>
2021-03-07 17:55:57 -0300

    inclusao serialize e api

--- view/src/components/ArtigoBusca.vue
+++ view/src/components/ArtigoBusca.vue
@@ -1,10 +1,27 @@
 <template>
-  <!-- <input type="text" /> -->
-  <span>busca</span>
+  <form action="">
+    <input name="busca" id="busca" v-model="busca" type="text" />
+    <input
+      id="lupa"
+      type="submit"
+      value="buscar"
+      @click.prevent="buscarListas"
+    />
+  </form>
 </template>
 <script>
 export default {
   name: "ArtigoBusca",
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarListas() {
+      this.$router.push({ query: { q: this.busca } });
+    },
+  },
 };
 </script>
 <style scope></style>
--- view/src/components/ArtigoLista.vue
+++ view/src/components/ArtigoLista.vue
@@ -1,45 +1,61 @@
 <template>
   <section class="produtos-container">
-    <div v-for="lista in listas" :key="lista.ID">
-      <h2 class="materia">
-        {{ lista.MATERIA }}
-      </h2>
-      <div>
-        <label for="">
-          {{ lista.DATA.slice(0, 10) }}
-        </label>
-        <span>
-          {{ lista.TIPO }}
-        </span>
-        <h3>
-          {{ lista.TITULO }}
-        </h3>
-        <p>
-          {{ lista.TEXTO }}
-        </p>
-        <img v-if="lista.FOTO" :src="lista.FOTO" alt="" />
-        <div class="youtube-video">
-          <iframe
-            width="674"
-            height="380"
-            :src="lista.VIDEO"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
-        </div>
+    <div v-if="listas && listas.length" class="listas">
+      <div v-for="lista in listas" class="lista" :key="lista.ID">
+        <router-link to="/">
+          <h2 class="materia">
+            {{ lista.MATERIA }}
+          </h2>
+          <div>
+            <label>
+              {{ lista.DATA.slice(0, 10) }}
+            </label>
+            <span>
+              {{ lista.TIPO }}
+            </span>
+            <h3>
+              {{ lista.TITULO }}
+            </h3>
+            <p>
+              {{ lista.TEXTO }}
+            </p>
+            <img v-if="lista.FOTO" :src="lista.FOTO" alt="" />
+            <div class="youtube-video">
+              <iframe
+                width="674"
+                height="380"
+                :src="lista.VIDEO"
+                frameborder="0"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+        </router-link>
       </div>
+    </div>
+    <div v-else-if="listas && listas.length === 0">
+      <p>Busca sem resultados. Tente buscar outro termo.</p>
     </div>
   </section>
 </template>
 <script>
 import { api } from "@/services.js";
+// import { serialize } from "@/helpers.js";
+
 export default {
   name: "ArtigoLista",
   data() {
     return {
       listas: null,
+      listasPorPagina: 9,
     };
   },
+  // computed: {
+  //   url() {
+  //     const query = serialize(this.$route.query);
+  //     return `"/?_limit=${this.listasPorPagina}${query}"`;
+  //   },
+  // },
   methods: {
     getListas() {
       api
@@ -51,9 +67,22 @@ export default {
         .catch((err) => console.log(err));
     },
   },
+  // watch: {
+  //   url() {
+  //     this.getListas();
+  //   },
+  // },
   created() {
     this.getListas();
   },
 };
 </script>
-<style scope></style>
+<style scope>
+.listas {
+  display: block;
+}
+.lista {
+  border-top: 2px solid lightgrey;
+  border-bottom: 2px solid lightgrey;
+}
+</style>
